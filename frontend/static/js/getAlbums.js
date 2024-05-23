1f20fd047165eed04f6f7fcff07e48fa870d3b0b
Fabrice Werger <fabricewerger@icloud.com>
2024-05-23 11:50:22 +0200

    spa-001: cleanup functions

--- frontend/static/js/getAlbums.js
+++ frontend/static/js/getAlbums.js
@@ -1,7 +1,6 @@
 export async function getAlbums() {
-  // Authorization token that must have been created previously. See : https://developer.spotify.com/documentation/web-api/concepts/authorization
-  const token =
-    'BQAjEq-26k9RDidxW2s9YmsH3ry4zrZOQpuDBQQBD2ZgJOOnYi5eSISq9qmgZXXtN53fTZqTAOPSEs1VCUpiUaRMZP_SXacNCXQQUG2OXlTAioDI28fUOPDk3uwFFvKo3REWgi75qOmtaH19Q_dEX2WZE0x8t1n7xC1DTuIt2qKR11XNnNama0LhoAYPFfTuBNcY7jeMU-Kps-XVhbXBu3e_hGtzWvc1Th_lfRBxSAwX17Xu2iwm3R5iWnhqrUpXist81W-X'
+  // Authorization token that must have been created previously. See : https://developer.spotify.com/documentation/web-api/concepts/authorization // fill in from https://developer.spotify.com/
+  const token = ''
   async function fetchWebApi(endpoint, method, body) {
     const res = await fetch(`https://api.spotify.com/${endpoint}`, {
       headers: {
@@ -14,7 +13,6 @@ export async function getAlbums() {
   }
 
   async function getTopTracks() {
-    // Endpoint reference : https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks
     return (
       await fetchWebApi('v1/me/top/tracks?time_range=long_term&limit=5', 'GET')
     ).items
