d73bcbb1e20bdbe18b888191215e73b88516579c
Fabrice Werger <fabricewerger@icloud.com>
2024-06-24 17:09:29 +0200

    spa-011: added genre based background colour

--- frontend/static/js/getTopFive.js
+++ frontend/static/js/getTopFive.js
@@ -28,42 +28,77 @@ function fetchWebApi(endpoint, method, body) {
 }
 
 function getTopTracks() {
-  return fetchWebApi('v1/me/top/tracks?time_range=long_term&limit=5', 'GET')
+  return fetchWebApi('v1/me/top/tracks?time_range=short_term&limit=5', 'GET')
 }
 
-const getTopFive = () => {
+async function getArtistGenres(artistId) {
+  return fetchWebApi(`v1/artists/${artistId}`, 'GET')
+    .then((artist) => artist.genres)
+    .catch((error) => {
+      console.error('Failed to fetch artist genres:', error)
+      return []
+    })
+}
+
+const genreToClass = {
+  'alternative rock': 'genre-alternative-rock',
+  'ambient folk': 'genre-ambient-folk',
+  'modern rock': 'genre-modern-rock',
+  'electro house': 'genre-electro-house',
+}
+
+const getTopFive = async () => {
   const trackListContainer = document.getElementById('trackList')
   const spinner = document.createElement('div')
   spinner.className = 'spinner'
   trackListContainer.innerHTML = ''
   trackListContainer.appendChild(spinner)
 
-  Promise.all([getTopTracks(), waitaMoment()])
-    .then(([res]) => {
-      const topTracks = res.items
-      const trackList = topTracks?.map(
-        ({ name, artists }) =>
-          `${name} by ${artists.map((artist) => artist.name).join(', ')}`,
-      )
+  try {
+    const [res] = await Promise.all([getTopTracks(), waitaMoment()])
+    const topTracks = res.items
+    trackListContainer.innerHTML = ''
 
-      trackListContainer.innerHTML = ''
+    for (const { name, artists, album, popularity } of topTracks) {
+      const genres = (
+        await Promise.all(artists.map((artist) => getArtistGenres(artist.id)))
+      ).flat()
+      const listItem = document.createElement('li')
+      listItem.classList.add('track-item')
 
-      trackList.forEach((track) => {
-        const listItem = document.createElement('li')
-        listItem.textContent = track
-        trackListContainer.appendChild(listItem)
+      genres.forEach((genre) => {
+        const genreClass = genreToClass[genre]
+        if (genreClass) {
+          listItem.classList.add(genreClass)
+        }
       })
-    })
-    .catch((rej) => {
-      console.error('Error in getTopFive:', rej)
-      trackListContainer.innerHTML =
-        '<li>Failed to load top tracks. Please try again later.</li>'
-    })
-    .finally(() => {
-      if (spinner.parentNode === trackListContainer) {
-        trackListContainer.removeChild(spinner)
-      }
-    })
+
+      const trackInfo = document.createElement('div')
+      trackInfo.classList.add('track-item-info')
+      const trackImage = document.createElement('img')
+      trackImage.classList.add('track-item-image')
+
+      trackInfo.innerHTML = `
+        <span class='track-item-info-title'>${name}</span>
+        <br><span>${artists.map((artist) => artist.name).join(', ')}</span>
+        <br><span>Popularity: ${popularity}</span>
+        <br><span>Genres: ${genres.join(', ')}</span>`
+      trackImage.src = album.images[1]?.url
+      trackImage.alt = name
+
+      listItem.appendChild(trackImage)
+      listItem.appendChild(trackInfo)
+      trackListContainer.appendChild(listItem)
+    }
+  } catch (error) {
+    console.error('Error in getTopFive:', error)
+    trackListContainer.innerHTML =
+      '<li>Failed to load top tracks. Please try again later.</li>'
+  } finally {
+    if (spinner.parentNode === trackListContainer) {
+      trackListContainer.removeChild(spinner)
+    }
+  }
 }
 
 window.getTopFive = getTopFive
--- frontend/static/js/views/Test.js
+++ frontend/static/js/views/Test.js
@@ -11,13 +11,6 @@ export default class extends AbstractView {
       <h1>Test</h1>
       <p>testpage</p>
       <a onclick="getTopFive()">Show favorite songs</a>
-      <br>
-      <a onclick="getRandomNumber()">random nummer promise</a>
-      <br>
-      <h2>Random Number Result:</h2>
-      <div id="randomNumberResult">Click to get random number</div>
-
-
 
       <h2>Top Tracks:</h2>
       <ul id="trackList"></ul>
